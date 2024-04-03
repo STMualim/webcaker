009d9bb66aa88624d1cb6d6c62c6420d3c82061d
mohimamrifai <imamrifaibisnis@gmail.com>
2024-04-03 21:54:16 +0700

    fixing opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   metadataBase: new URL('http://localhost:3000'),
-  title: "Cari Kerja App",
-  description: "Cari kerja mudah dimana aja!!!",
+  title: "CAKER ID | Cari Kerja App",
+  description: "CAKER ID adalah aplikasi inovatif yang memudahkan pencarian pekerjaan bagi siapa pun, di mana pun mereka berada. Dengan CAKER ID, Anda tidak perlu lagi menghabiskan waktu berjam-jam untuk mencari peluang pekerjaan yang sesuai. Aplikasi ini dirancang untuk memberikan kemudahan dalam menemukan pekerjaan yang tepat, sesuai dengan keahlian dan preferensi Anda.",
   openGraph: {
     images: '/banner.png',
   },
